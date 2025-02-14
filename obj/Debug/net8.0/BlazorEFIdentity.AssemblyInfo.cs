115a2ed5a85bd7b3ef78442cce8d8990eb174cc4
Qwanie <143743954+Qwanie@users.noreply.github.com>
2025-02-14 10:35:11 +0100

    Kontosida med funktionalitet.

--- obj/Debug/net8.0/BlazorEFIdentity.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorEFIdentity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorEFIdentity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4adf74032947214c752c8bc87f56ad1af1ae9169")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ca2b49e536c89a5bfaadc8a4f5e900dd749db29")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorEFIdentity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorEFIdentity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
